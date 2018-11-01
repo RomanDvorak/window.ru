b0d60ac18d55c1f8927e2e5b95eff8de4a301911
RomanDvorak <41663545+RomanDvorak@users.noreply.github.com>
2018-11-01 21:17:29 +0300

    Модальное окно, связанное с телефонами

--- window.ru/js/script.js
+++ window.ru/js/script.js
@@ -1,12 +1,11 @@
 window.addEventListener('DOMContentLoaded', function() {
 
   'use strict';
-
+  // first popup
   let callEngineerBtn = document.querySelector('.popup_engineer_btn'),
       engineerPopup = document.querySelector('.popup_engineer'),
       engineerPopupDialog = engineerPopup.querySelector('.popup_dialog'),
-      engineerPopupContent = engineerPopupDialog.querySelector('.popup_content'),
-      engineerPopupClose = engineerPopupContent.querySelector('.popup_close');
+      engineerPopupContent = engineerPopupDialog.querySelector('.popup_content');
 
   callEngineerBtn.addEventListener('click', function() {
     engineerPopup.style.display = 'block';
@@ -16,4 +15,22 @@ window.addEventListener('DOMContentLoaded', function() {
       engineerPopup.style.display = 'none';
     }
   });
+
+  // phones
+
+  let phones = document.querySelectorAll('.phone_link'),
+      phonesPopup = document.querySelector('.popup');
+
+  for (let i = 0; i < phones.length; i++) {
+    phones[i].addEventListener('click', function(e) {
+      e.preventDefault();
+      phonesPopup.style.display = 'block';
+    });
+  }
+  document.addEventListener('click', function(e) {
+    if(e.target.parentNode.classList.contains('popup_close') || e.target.classList.contains('popup')) {
+      phonesPopup.style.display = 'none';
+    }
+  });
+
 });
